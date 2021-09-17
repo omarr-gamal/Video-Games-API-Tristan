7ddcb0040baccd6c611914ffff8fcc08aea836e8
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-09-17 20:05:07 +0200

    added env variables for database url

--- models.py
+++ models.py
@@ -5,16 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# database_name = "tristan"
-# db_user = os.environ['DB_USER']
-# db_pass = os.environ['DB_PASS']
-# database_path = os.environ['DATABASE_URL']
-dev_database_path = database_path = "postgresql://postgres:gsaiho2017@localhost:5432/tristan"
-prod_database_path = 'postgresql://ebulgcmkogfscf:d516f4d3a4f3ece5f8710ac6ebc9be05fbaec90b226932ecb2b9b15044abbe58@ec2-18-215-44-132.compute-1.amazonaws.com:5432/d2n6j67332m9ll'
+environment = os.environ['ENVIRONMENT']
+database_path = ''
+
+if environment == 'dev':
+    database_name = "tristan"
+    db_user = os.environ['DB_USER']
+    db_pass = os.environ['DB_PASS']
+    database_path = "postgresql://{}:{}@{}/{}".format(db_user, db_pass,
+                                                      'localhost:5432', database_name)
+elif environment == 'prod':
+    database_path = os.environ['DB_URL']
 
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = dev_database_path
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
